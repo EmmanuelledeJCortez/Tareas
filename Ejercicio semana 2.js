abce3abcf13ce6db959f21f1d263b92f59661cf6
EmmanuelledeJCortez <cortezemmanuelle@gmail.com>
2021-02-06 14:27:08 -0600

    second commit

---
+++ Ejercicio semana 2.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2021 by anonymous (http://jsbin.com/mibifadisa/3/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+
+<script id="jsbin-javascript">
+var mes=prompt("¿Qué mes es?")
+    
+if (mes==1||mes==2||mes==12){
+  console.log("Es Invierno")
+  alert("Es Invierno")
+  
+}else if(mes==3||mes==4||mes==5){
+  console.log("Es Primvera")
+  alert("Es Primavera")
+  
+}else if(mes==6||mes==7||mes==8){
+  console.log("Es Verano")
+  alert("Es Verano")
+  
+}else if(mes==9||mes==10||mes==11){
+  console.log("Es Otoño")
+  alert("Es Otoño")};
+
+alert("21005384")
+  
+  
+
+
+</script>
+</body>
+</html>
